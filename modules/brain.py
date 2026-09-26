5482376698aa133a87563a4e2fd58f7319a4c1ac
Imperiussss <Ptvu1210@gmail.com>
2026-09-26 17:55:27 +0700

    Update Search Command Feature

--- modules/brain.py
+++ modules/brain.py
@@ -1,9 +1,12 @@
 import json
 import webbrowser
 from AppOpener import open as open_app
-from datetime import date
-from datetime import datetime
+from AppOpener import close as close_app
+from datetime import date, datetime
 from modules.listener import robot_ear
+from urllib.parse import quote_plus
+
+# ================================== READ KEYWORD ==================================
 
 with open("data/apps.json", "r", encoding="utf-8") as app_file:
     apps = json.load(app_file)
@@ -11,45 +14,83 @@ with open("data/apps.json", "r", encoding="utf-8") as app_file:
 with open("data/webs.json", "r", encoding="utf-8") as web_file:
     webs = json.load(web_file)
 
+# ===================================== COMMAND =====================================
+
+OPEN_COMMANDS =  {
+    "open", "launch", "start", "run"
+}
+
+SEARCH_COMMANDS = { 
+    "search", "google", "look up"
+}
+
+CLOSE_COMMANDS = { 
+    "close", "exit", "quit"
+}
+
+# ===================================== UTILS ===================================== 
+
+def contains_command(sentences, commands):
+    return any(word in sentences for word in commands)
+
+
+# ===================================== COMMAND ( BRAIN ) ===================================== 
+
 def conversation_command(command):
     if "hello" in command or "hi" in command:
-        robot_brain = "Hello, Thanh Vu"
+        return "Hello, Thanh Vu"
     elif "today" in command:
         today = date.today()
-        robot_brain = "Today is: " + today.strftime("%B %d, %Y")
+        return "Today is: " + today.strftime("%B %d, %Y")
     elif "time" in command:
         time = datetime.now()
-        robot_brain = "Current time is: " + time.strftime("%H hours %M minutes %S seconds")
+        return "Current time is: " + time.strftime("%H hours %M minutes %S seconds")
     elif "how are you" in command:
-        robot_brain = "Im fine thank you, and you?"
+        return "Im fine thank you, and you?"
     elif any(keyword in command for keyword in ("bye","goodbye","close")):
-        robot_brain = "Goodbye!"
+        return "Goodbye!"
     else:
-        robot_brain = "Sorry, i can't understand"
-    return robot_brain
+        return "Sorry, i can't understand"
 
 
 def application_command(command):
     for keyword in sorted(apps, key=len, reverse=True):
         app_name = apps[keyword]
-        if keyword in command:
-            robot_brain = "Open " + keyword
+
+        if contains_command(command, OPEN_COMMANDS) and keyword in command:
             open_app(app_name)
-            return robot_brain
+            return "Open " + keyword
+        elif contains_command(command, CLOSE_COMMANDS) and keyword in command:
+            close_app(app_name)
+            return "Close " + keyword
         
     return "I can't find this Application"
 
 def website_command(command):
     for keyword in sorted(webs, key=len, reverse=True):
         web_name = webs[keyword]
-        if keyword in command:
-            robot_brain = "Open " + keyword
+
+        if contains_command(command, OPEN_COMMANDS) and keyword in command:
             webbrowser.open(web_name)
-            return robot_brain
+            return "Open " + keyword
         
     return "I can't find this Website"
 
 
+def search_command(command):
+    for keyword in SEARCH_COMMANDS:
+        if command.startswith(keyword):
+            query = command[len(keyword):].strip()
+
+            if query:
+                encoded_query = quote_plus(query)
+                url = "https://www.google.com/search?q=" + encoded_query
+                webbrowser.open(url)
+                return command + query
+
+    return "What do you want me to search?"
+
+# ===================================== LEARN ===================================== 
 
 def learn(audio):
     try:
@@ -60,14 +101,13 @@ def learn(audio):
     print(command)
     
     if command == "":
-        robot_brain = "Sorry, i can't hear you"            
-    elif "open" in command:
-        if any(keyword in command for keyword in webs):
-            robot_brain = website_command(command)
-        elif any(keyword in command for keyword in apps):
-            robot_brain = application_command(command)    
-        else: 
-            robot_brain = "I can't find this website or application"
+        robot_brain = "Sorry, i can't hear you"   
+    elif any(command.startswith(keyword) for keyword in SEARCH_COMMANDS):
+            robot_brain = search_command(command)             
+    elif any(keyword in command for keyword in webs):
+        robot_brain = website_command(command)
+    elif any(keyword in command for keyword in apps):
+        robot_brain = application_command(command)
     else: 
         robot_brain = conversation_command(command)
     return robot_brain
