71c222bbc15cb97394284d6af8eac94384298bc5
Imperiussss <Ptvu1210@gmail.com>
2026-09-26 17:56:20 +0700

    Change listen duration

--- modules/listener.py
+++ modules/listener.py
@@ -5,6 +5,6 @@ robot_ear = speech_recognition.Recognizer()
 def listen():
     with speech_recognition.Microphone() as mic:
         print("Robot: I'm listening")
-        audio = robot_ear.record(mic, duration=3)
+        audio = robot_ear.record(mic, duration=4)
         
     return audio
